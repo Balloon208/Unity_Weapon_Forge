022f3c7484be9adebfeb7a70d2f642f4e63e599f
Balloon208 <gnswldms2@gmail.com>
2023-07-02 23:51:59 +0900

    FIXED :: UpgradeShopBar
    1. UpgradeShopBar's color is now work well!

--- Solo Project/Assets/Script/UpgradeUI.cs
+++ Solo Project/Assets/Script/UpgradeUI.cs
@@ -5,16 +5,36 @@ using UnityEngine.UI;
 
 public class UpgradeUI : MonoBehaviour
 {
+    private Image[] image = new Image[3];
+    public Color Oncolor;
+    public Color Offcolor;
     public GameObject[] UI = new GameObject[3];
+    public GameObject[] BackgroundObject = new GameObject[3];
 
     // Update is called once per frame
+
+    public void Start()
+    {
+        for(int i=0; i<3; i++)
+        {
+            image[i] = BackgroundObject[i].GetComponent<Image>();
+        }
+    }
     public void ChangeUI(int n)
     {
+        bool locked = false;
         for(int i = 0; i < 3; i++)
         {
-            if(i == n) UI[i].SetActive(true);
-            else UI[i].SetActive(false);
+            if (i == n)
+            {
+                UI[i].SetActive(true);
+                image[i].color = Oncolor;
+            }
+            else
+            {
+                UI[i].SetActive(false);
+                image[i].color = Offcolor;
+            }
         }
-        
     }
 }
